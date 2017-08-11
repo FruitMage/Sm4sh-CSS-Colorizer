ee41e2ee15e9c3785517b1ef252bf8313218b30a
FruitMage <fruitymage@gmail.com>
2017-08-11 16:13:11 -0500

    Program icon by Tiniest Turtles

--- Sm4sh CSS Colorizer/Form1.Designer.cs
+++ Sm4sh CSS Colorizer/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.open = new System.Windows.Forms.ToolStripMenuItem();
@@ -451,6 +452,7 @@
             this.ClientSize = new System.Drawing.Size(810, 324);
             this.Controls.Add(this.colors);
             this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.MaximumSize = new System.Drawing.Size(826, 363);
             this.MinimumSize = new System.Drawing.Size(826, 363);
